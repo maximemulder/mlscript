7443843683ef9d0c66394bbc25d051d508299dbd
Maxime Mulder <maxime-mulder@outlook.com>
2025-05-15 19:23:04 +0800

    add pattern matching syntax

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/direction.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/direction.scala
@@ -3,18 +3,18 @@ package hkmc2.ctml.core
 import hkmc2.ctml.types.*
 
 /** Combine two types according to a given type direction. */
-def combine(left: Type, right: Type, dir: Direction)(using ctx: Context) =
+def combine(left: Type, right: Type, dir: Direction)(using ctx: Context): Type =
   dir match
     case Direction.Sub   => meet(left, right)
     case Direction.Super => join(left, right)
 
 /** Combineof many types according to a given type direction. */
-def combineMany(types: List[Type], dir: Direction)(using ctx: Context) =
+def combineMany(types: List[Type], dir: Direction)(using ctx: Context): Type =
   val extremalType = getExtremalType(dir)
   types.foldRight(extremalType)(combine(_, _, dir))
 
 /** Get the extremal type of a given type direction. */
-def getExtremalType(dir: Direction) =
+def getExtremalType(dir: Direction): Type =
   dir match
     case Direction.Sub   => TTop
     case Direction.Super => TBot

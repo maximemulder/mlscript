a6d842687e54bc3cedae494c86b1d6b2ef4e525f
Maxime Mulder <maxime-mulder@outlook.com>
2025-05-21 16:47:14 +0800

    handle declarations

---
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/test/Stmt.scala
@@ -0,0 +1,29 @@
+package hkmc2.ctml.test
+
+import hkmc2.ctml.types.*
+
+/** A CTML statement, which is used in the type system. */
+sealed trait Stmt
+
+/** An expression. */
+case class StmtExpr(expr: Expr) extends Stmt
+
+/** A variable declaration. */
+case class StmtExprDecl(name: String, type_ : Type) extends Stmt
+
+/** A type declaration. */
+case class StmtTypeDecl(name: String) extends Stmt
+
+/** A relation between two types. */
+case class StmtTypeRel(rel: TypeRel, left: Type, right: Type) extends Stmt
+
+/** A relation between two types. */
+enum TypeRel:
+  /** The two types are equal. */
+  case Eq
+  /** The two types are not equal. */
+  case Ne
+  /** The left type is a subtype of the right type. */
+  case Sub
+  /** The left type is a supertype of the right type. */
+  case Sup
--- hkmc2/shared/src/main/scala/hkmc2/ctml/test/test.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/test/test.scala
@@ -5,57 +5,87 @@ import hkmc2.ctml.types.*
 import hkmc2.semantics.Term
 
 /** Run a CTML test on an input term. */
-def test(term: Term, output: (String) => Unit): Unit =
-  // Assign global CTML debug output function.
-  outputter = output
+def test(term: Term, ctx: Context, output: (String) => Unit): Context =
+  val tester = Tester(ctx, output)
+  tester.test(term)
+  tester.ctx
 
-  // Try to parse the input term.
-  val meta = try
-    parseMeta(term)
-  catch
-    case error: ParseError =>
-      output(s"PARSE ERROR: ${error.getMessage()}")
-      return
+class Tester(var ctx: Context, output: (String) => Unit):
+  /** Run a CTML test on an input term. */
+  def test(term: Term): Unit =
+    // Assign global CTML debug output function.
+    outputter = output
 
-  try
-    // Assign the global CTML fresh variable counter.
-    freshVarCounter = 0
+    // Try to parse the input term.
+    val stmts = try
+      parseStmts(term)
+    catch
+      case error: ParseError =>
+        this.output(s"PARSE ERROR: ${error.getMessage()}")
+        return
 
+    try
+      // Assign the global CTML fresh variable counter.
+      freshVarCounter = 0
+
+      for stmt <- stmts do
+        testStatement(stmt)
+    catch
+      case error: TypeError =>
+        output(s"TYPE ERROR: ${error.getMessage()}")
+
+  /** Test a statement */
+  def testStatement(stmt: Stmt) =
     // Run the type inference or type constraining test.
-    val bounds = meta match
-      case MetaExpr(expr) =>
-        testExpr(expr, output)
-      case MetaType(rel, left, right) =>
-        testTypeRel(rel, left, right, output)
+    stmt match
+      case StmtExprDecl(name, type_) =>
+        this.addExprDecl(name, type_)
+      case StmtTypeDecl(name) =>
+        this.addTypeDecl(name)
+      case StmtExpr(expr) =>
+        this.testExpr(expr)
+      case StmtTypeRel(rel, left, right) =>
+        this.testTypeRel(rel, left, right)
 
-    // Print the generated type bounds if there are some.
+  /** Add a variable declaration to the context. */
+  def addExprDecl(name: String, type_ : Type) =
+    this.ctx = CtxVar(name, type_) :: this.ctx
+
+  /** Add a type declaration to the context. */
+  def addTypeDecl(name: String) =
+    this.ctx = CtxTypeVar(name, TypeVarKind.Rigid) :: this.ctx
+
+  /** Test an expression type inference. */
+  def testExpr(expr: Expr) =
+    val (type_, bounds) = infer(expr, this.ctx)
+    this.outputType(type_)
+    this.outputBounds(bounds)
+
+  /** Test the relation between two types. */
+  def testTypeRel(rel: TypeRel, left: Type, right: Type) =
+    given Context = Context.primitive
+    val bounds = rel match
+      case TypeRel.Eq =>
+        if !checkEq(left, right) then
+          throw new TypeError("FAIL")
+        Nil
+      case TypeRel.Ne =>
+        if checkEq(left, right) then
+          throw new TypeError("FAIL")
+        Nil
+      case TypeRel.Sub =>
+        constrainSub(left, right)
+      case TypeRel.Sup =>
+        constrainSub(right, left)
+
+    this.output("OK")
+    this.outputBounds(bounds)
+
+  /** Output the inferred type. */
+  def outputType(type_ : Type) =
+    this.output(type_.show())
+
+  /** Output the generated type bounds if there are some. */
+  def outputBounds(bounds: List[Bound]) =
     if bounds != Nil then
-      output(showBounds(bounds))
-  catch
-    case error: TypeError =>
-      output(s"TYPE ERROR: ${error.getMessage()}")
-
-/** Run a CTML type inference test on an expression. */
-def testExpr(expr: Expr, output: (String) => Unit): List[Bound] =
-  val (type_, bounds) = infer(expr, Context.primitive)
-  output(type_.show())
-  bounds
-
-/** Run a CTML type relation test on two types. */
-def testTypeRel(rel: TypeRel, left: Type, right: Type, output: (String) => Unit): List[Bound] =
-  given Context = Context.primitive
-  val bounds = rel match
-    case TypeRel.Eq =>
-      if !checkEq(left, right) then
-        throw new TypeError("FAIL")
-      Nil
-    case TypeRel.Ne =>
-      if checkEq(left, right) then
-        throw new TypeError("FAIL")
-      Nil
-    case TypeRel.Sub =>
-      constrainSub(left, right)
-    case TypeRel.Sup =>
-      constrainSub(right, left)
-  output("OK")
-  bounds
+      this.output(showBounds(bounds))

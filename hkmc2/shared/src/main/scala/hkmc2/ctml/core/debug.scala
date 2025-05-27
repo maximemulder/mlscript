87c3dbb366c59da1961f4aa19433cc46b761fec1
Maxime Mulder <maxime-mulder@outlook.com>
2025-05-27 12:43:57 +0800

    fix some bugs

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/debug.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/debug.scala
@@ -8,3 +8,18 @@ var tab = 0
 
 def debug(value : Any) =
   outputter(("  " * tab) + value.toString())
+
+
+def debugConstrainSub(impl: (Type, Type) => Clauses, sub: Type, sup: Type)(using ctx: Clauses, mode: Mode): Clauses =
+  try
+    val outs = try
+      tab += 1
+      constrainSubImpl(sub, sup)
+    finally
+      tab -= 1
+    debug(s"${mode} ${sub} ≤ ${sup} OK ${outs}")
+    outs
+  catch
+    case error: TypeError =>
+      debug(s"${mode} ${sub} ≤ ${sup} FAIL")
+      throw error

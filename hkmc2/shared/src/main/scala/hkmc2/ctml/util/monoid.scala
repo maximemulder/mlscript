e64c2db19f5e5547b3a00f5aa6a95d384efd86eb
Maxime Mulder <maxime-mulder@outlook.com>
2025-07-04 16:08:48 +0800

    overengineered get type variables

---
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/util/monoid.scala
@@ -0,0 +1,23 @@
+package hkmc2.ctml.util
+
+/** The monoid trait. */
+trait Monoid[T]:
+  /** The identity element. */
+  def empty: T
+
+  /** The associative operation. */
+  def combine(a: T, b: T): T
+
+/** The set implementation of the monoid trait. */
+implicit def SetMonoid[T]: Monoid[Set[T]] = new Monoid[Set[T]] {
+  def empty = Set.empty
+  def combine(a: Set[T], b: Set[T]): Set[T] =
+    a.concat(b)
+}
+
+/** The list implementation of the monoid trait. */
+implicit def ListMonoid[T]: Monoid[List[T]] = new Monoid[List[T]] {
+  def empty = List.empty
+  def combine(a: List[T], b: List[T]): List[T] =
+    a.concat(b)
+}

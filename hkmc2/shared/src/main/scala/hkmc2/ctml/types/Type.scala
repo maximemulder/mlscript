5c659421b755df32cae6729b3e219c1ef0c06301
Maxime Mulder <maxime-mulder@outlook.com>
2025-07-04 17:52:36 +0800

    more typeclasses

--- hkmc2/shared/src/main/scala/hkmc2/ctml/types/Type.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/types/Type.scala
@@ -1,5 +1,7 @@
 package hkmc2.ctml.types
 
+import hkmc2.ctml.util.*
+
 /** A type. */
 sealed trait Type:
   /** Get the string representation of the object. */
@@ -30,5 +32,30 @@ case class TConstrained(val vars: List[TypeVar], val base: Type, val bounds: Lis
 /** A constraing type. */
 case class TConstraining(val base: Type, val bounds: List[Bound]) extends Type
 
+/** The top type type alias. */
 type TBot = TBot.type
+
+/** The bottom type type alias. */
 type TTop = TTop.type
+
+extension (type_ : Type)
+  /** Get the components of a type. */
+  def components: List[Type] =
+    type_ match
+      case TBot | TTop | TVar(_) =>
+        Nil
+      case TLam(param, ret) =>
+        List(param, ret)
+      case TUnion(left, right) =>
+        List(left, right)
+      case TInter(left, right) =>
+        List(left, right)
+      case TConstrained(_, base, bounds) =>
+        bounds.map(_.type_) :+ base
+      case TConstraining(base, bounds) =>
+        bounds.map(_.type_) :+ base
+
+/** The type implementation of the tree trait. */
+implicit def TypeTree: Tree[Type] = new Tree[Type] {
+  def children(type_ : Type) = type_.components
+}

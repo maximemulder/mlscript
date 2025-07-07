acdb888cde428dd18eded18044b0acf506be0b61
Maxime Mulder <maxime-mulder@outlook.com>
2025-07-07 19:57:25 +0800

    fix tree printing

--- hkmc2/shared/src/main/scala/hkmc2/ctml/util/Tree.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/util/Tree.scala
@@ -8,10 +8,13 @@ trait Tree[T]:
 /** Implementation of the `Show` trait for `Tree`. */
 def TreeShow[T](using t: Tree[T], s: Show[T]): Show[T] = new Show:
   override def show(tree: T): String =
-    val builder = StringBuilder(s.show(tree))
-    val children = t.children(tree)
-    if children != List.empty then
-      builder.append(" ")
-      builder.append(children)
-
+    val builder = StringBuilder()
+    showTree(tree, 0, builder)
     builder.toString()
+
+  /** Convert a tree to a string at a given level. */
+  private def showTree(tree: T, level: Int, builder: StringBuilder): Unit =
+    builder.append(s.show(tree).addIndentation(level))
+    builder.append("\n")
+    for child <- t.children(tree) do
+      showTree(child, level + 1, builder)

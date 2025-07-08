c3158a9121addea79e56e54f4f650b0da17b21a7
Maxime Mulder <maxime-mulder@outlook.com>
2025-07-08 23:23:32 +0800

    factorize and simplify type fold

---
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/fold.scala
@@ -0,0 +1,14 @@
+package hkmc2.ctml.core.type_
+
+import hkmc2.ctml.types.*
+import hkmc2.ctml.util.*
+
+extension (type_ : Type)
+  /** Apply a fold on the direct components of a type. */
+  def fold[T](acc: T)(f: (Type, T) => T) =
+    type_.components.foldRight(acc)(f)
+
+extension (type_ : Type)
+  /** Accumulate a monoidic value on the direct components of a type. */
+  def accumulate[T](f: Type => T)(using m: Monoid[T]): T =
+    type_.fold(m.empty)((type_, acc) => m.combine(acc, f(type_)))

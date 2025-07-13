ec70b1c8e61e690981fdee206eb6640def83adb1
Maxime Mulder <maxime-mulder@outlook.com>
2025-07-13 22:28:10 +0800

    better abstractions with functors and foldables

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/hasVar.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/hasVar.scala
@@ -1,8 +1,10 @@
 package hkmc2.ctml.core.type_
 
 import hkmc2.ctml.core.*
+import hkmc2.ctml.core.debug.*
 import hkmc2.ctml.types.*
 import hkmc2.ctml.util.*
+import hkmc2.ctml.util.given
 
 extension (clauses: Clauses)
   /** Check whether a type variable appears in the clauses. */
@@ -35,5 +37,9 @@ extension (type_ : Type)
     type_ match
       case TVar(typeVar) if typeVar == var_ =>
         true
+      case TConstrained(vars, base, bounds) =>
+        base.hasVar(var_) || bounds.map(bound => if bound.var_ == var_ then false else bound.type_.hasVar(var_)).foldM()
+      case TConstraining(base, bounds) =>
+        base.hasVar(var_) || bounds.map(_.hasVar(var_)).foldM()
       case _ =>
         type_.accumulate(_.hasVar(var_))

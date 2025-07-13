fa49a8a7700876fe934ea7ca4eced0b9087acabc
Maxime Mulder <maxime-mulder@outlook.com>
2025-07-13 19:07:18 +0800

    typo in comment

--- hkmc2/shared/src/main/scala/hkmc2/ctml/util/Order.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/util/Order.scala
@@ -2,9 +2,9 @@ package hkmc2.ctml.util
 
 /** The result of an order comparison. */
 enum Order:
-  /** The value is lesser than the right value. */
+  /** The left value is lesser than the right value. */
   case Lesser
   /** The left value is equal to the right value. */
   case Equal
-  /** The value is lesser than the right value. */
+  /** The left value is greater than the right value. */
   case Greater

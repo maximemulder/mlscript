889a60552890cbd8a5b11671b3996a3226d27e7c
Maxime Mulder <maxime-mulder@outlook.com>
2025-07-16 00:10:36 +0800

    better level variables

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/getVars.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/getVars.scala
@@ -1,14 +1,15 @@
 package hkmc2.ctml.core.type_
 
+import hkmc2.ctml.core.context.*
 import hkmc2.ctml.types.*
 import hkmc2.ctml.util.*
 import hkmc2.ctml.util.given
 
 extension (type_ : Type)
   /** Get the type variables referenced in a type. */
-  def getVars(): Set[TypeVar] =
+  def getVars()(using ctx: Context): Set[TypeVar] =
     type_ match
-      case TVar(var_) =>
+      case TVar(var_) if !ctx.isVarClass(var_) =>
         Set(var_)
       case _ =>
         type_.accumulate(_.getVars())

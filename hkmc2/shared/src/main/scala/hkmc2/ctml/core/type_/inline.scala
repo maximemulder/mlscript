ae9fbf1aa2476ee5b73d397e00a1ebbdd9cd6fb4
Maxime Mulder <maxime-mulder@outlook.com>
2025-07-15 14:28:32 +0800

    finally freshen constrained types in subtyping

---
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/inline.scala
@@ -0,0 +1,35 @@
+package hkmc2.ctml.core.type_
+
+import hkmc2.ctml.core.*
+import hkmc2.ctml.types.*
+
+extension (type_ : Type)
+  /** Replace a type variable by a substitute type in a type, simplifying the resulting type if possible. */
+  def inline(var_ : TypeVar, substitute: Type)(using ctx: Context): Type =
+    type_ match
+      case TVar(typeVar) if typeVar == var_ =>
+        substitute
+      case TConstrained(vars, base, bounds) =>
+        val newBase   = base.inline(var_, substitute)
+        val newBounds = bounds.inline(var_, substitute)
+        TConstrained(vars, newBase, newBounds)
+      case TConstraining(base, bounds) =>
+        val newBase   = base.inline(var_, substitute)
+        val newBounds = bounds.inline(var_, substitute)
+        attachConstrainingBounds(newBase, newBounds)
+      case _ =>
+        type_.mapSimplify(_.inline(var_, substitute))
+
+extension (bounds: List[Bound])
+  /** Substitute a type variable by a type in the list of bounds. */
+  def inline(var_ : TypeVar, substitute: Type)(using ctx: Context): List[Bound] =
+    bounds.iterator
+      .filter(_.var_ != var_)
+      .map(_.inline(var_, substitute))
+      .toList
+
+extension (bound: Bound)
+  /** Substitute a type variable by a type in the bound. */
+  def inline(var_ : TypeVar, substitute: Type)(using ctx: Context): Bound =
+    val newType = bound.type_.inline(var_, substitute)
+    Bound(bound.var_, bound.dir, newType)

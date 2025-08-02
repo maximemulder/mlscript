41c70854eb7a9b4e7ae1f5cc680a0e709b30ab2b
Maxime Mulder <maxime-mulder@outlook.com>
2025-08-03 00:08:37 +0800

    more basic documentation

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/substitute.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/substitute.scala
@@ -6,36 +6,31 @@ import hkmc2.ctml.types.*
 import hkmc2.ctml.util.*
 
 extension (type_ : Type)
-  /** Replace a type variable by an other type variable in the type, without simplifying the resulting type. */
+  /** Subsitute a type variable by another type variable in the type, without simplifying the resulting type. */
   def substitute(var_ : TypeVar, substitute: TypeVar): Type =
-    TypeSubstituter(type_, SubstitutionParams(var_, substitute))
+    TypeSubstitute(type_, TypeSubstituteParams(var_, substitute))
 
-class SubstitutionParams(val var_ : TypeVar, val substitute: TypeVar)
+/** Parameters of the type substitution operation. */
+class TypeSubstituteParams(val var_ : TypeVar, val substitute: TypeVar)
 
-object TypeSubstituter extends TypeDispatcher[Id, Id, SubstitutionParams](TypeIdentityCombinator[SubstitutionParams]):
-  override def apply(type_ : Type, params : SubstitutionParams): Type =
+/** Implementation of the type substitution operation. */
+object TypeSubstitute extends TypeDispatcher[Id, Id, TypeSubstituteParams](TypeIdentityCombinator[TypeSubstituteParams]):
+  override def apply(type_ : Type, params : TypeSubstituteParams): Type =
     type_ match
       case TVar(var_) =>
         TVar(this.substitute(var_, params))
       case _ =>
         super.apply(type_, params)
 
-  override def apply(bounds: List[Bound], params: SubstitutionParams): List[Bound] =
+  override def apply(bounds: List[Bound], params: TypeSubstituteParams): List[Bound] =
     bounds.map(bound =>
       val newVar  = this.substitute(bound.var_, params)
       val newType = this.apply(bound.type_, params)
       Bound(newVar, bound.dir, newType)
     )
 
-  def substitute(var_ : TypeVar, params: SubstitutionParams): TypeVar =
+  def substitute(var_ : TypeVar, params: TypeSubstituteParams): TypeVar =
       if var_ == params.var_ then
         params.substitute
       else
         var_
-
-extension (univ: TUniv)
-  /** Substitute the quantified variable of a universal type with a new fresh type variable. */
-  def freshen(): TUniv =
-    val freshDecl = declNewFreshVar()
-    val body = univ.body.substitute(univ.var_, freshDecl.var_)
-    TUniv(freshDecl.var_, body)

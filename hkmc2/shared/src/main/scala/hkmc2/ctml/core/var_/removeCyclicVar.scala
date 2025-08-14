f303cc5d49b8cafb8b1a1ae273be25da98a865c6
Maxime Mulder <maxime-mulder@outlook.com>
2025-08-15 02:10:49 +0800

    remove cyclic type variable constraints in subtyping

---
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/var_/removeCyclicVar.scala
@@ -0,0 +1,28 @@
+package hkmc2.ctml.core.var_
+
+import hkmc2.ctml.core.combine.*
+import hkmc2.ctml.core.debug.*
+import hkmc2.ctml.types.*
+
+extension (type_ : Type)
+  /** Remove cyclic type variable constraints in a type, that is, when a type variable is
+   *  constrained to be a subtype of supertype of itself.
+   *
+   *  The subtyping direction refers to the type variable on the left and the type on the right.
+   */
+  def removeCyclicVar(var_ : TypeVar, dir: Direction)(using Context): Type =
+    type_ match
+      case TVar(typeVar) if typeVar == var_ =>
+        getExtremalType(dir)
+      case TUnion(left, right) if dir == Direction.Super =>
+        join(
+          left.removeCyclicVar(var_, dir),
+          right.removeCyclicVar(var_, dir)
+        )
+      case TInter(left, right) if dir == Direction.Sub =>
+        meet(
+          left.removeCyclicVar(var_, dir),
+          right.removeCyclicVar(var_, dir)
+        )
+      case _ =>
+        type_

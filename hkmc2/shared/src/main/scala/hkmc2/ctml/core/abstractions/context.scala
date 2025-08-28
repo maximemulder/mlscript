56db82e179f55b88102d2bdf43317f49c4cbd8b7
Maxime Mulder <maxime-mulder@outlook.com>
2025-08-29 00:36:57 +0800

    factorize final combinator

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/abstractions/context.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/abstractions/context.scala
@@ -13,19 +13,18 @@ trait WithContext[This <: WithContext[This]]:
   def setContext(ctx: Context): This
 
 class TypeContextApplicator[T[+_], P <: WithContext[P]](
-  applicator: TypeApplicator[T, P] & BoundsApplicator[Id, P],
-  combinator: TypeCombinator[T, Id, P],
+  applicator: TypeApplicator[T, P] & BoundsApplicator[Id, P] & TypeNode[T, Id, P],
 ) extends TypeApplicator[T, P]:
   override def apply(type_ : Type, params: P): T[Type] =
     type_ match
       case TUniv(var_, body) =>
         val ctx = params.getContext.extend(declRigidVar(var_))
         val bodyRes = this.apply(body, params.setContext(ctx))
-        combinator.univ(var_, bodyRes, params)
+        applicator.getCombinator.univ(var_, bodyRes, params)
       case TConstrained(body, bounds) =>
         val ctx = params.getContext.extend(bounds)
         val bodyRes = this.apply(body, params.setContext(ctx))
         val boundsRes = applicator.apply(bounds, params)
-        combinator.constrained(bodyRes, boundsRes, params)
+        applicator.getCombinator.constrained(bodyRes, boundsRes, params)
       case _ =>
         applicator.apply(type_, params)

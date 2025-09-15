c65ea0e85fef235785ea4c54715d5df82608fdbf
Maxime Mulder <maxime-mulder@outlook.com>
2025-09-15 16:08:40 +0800

    dou6je5

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/hasVar.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/hasVar.scala
@@ -2,6 +2,7 @@ package hkmc2.ctml.core.type_
 
 import hkmc2.ctml.core.*
 import hkmc2.ctml.core.debug.*
+import hkmc2.ctml.core.type_.traits.*
 import hkmc2.ctml.types.*
 import hkmc2.ctml.util.*
 import hkmc2.ctml.util.given
@@ -33,15 +34,37 @@ extension (bound: Bound)
 extension (type_ : Type)
   /** Check whether a type variable appears in the type. */
   def hasVar(var_ : TypeVar): Boolean =
-    given Monoid[Boolean] = AnyMonoid
+    N1(type_, Params(var_))
+
+/** Parameters of the has variable operation. */
+private class Params(val var_ : TypeVar) extends WithTypeVar[Params]:
+  def getTypeVar = var_
+  def setTypeVar(var_ : TypeVar) = Params(var_)
+
+/** Shadowing node of the has variable operation. */
+private object N1 extends TypeShadowApplicator[Const[Boolean], Params](N2):
+  def univ(univ: TUniv): Boolean =
+    false
+
+/** Variable checking node of the has variable operation. */
+private object N2 extends TypeChainApplicator[Const[Boolean], Params](N3):
+  def apply(type_ : Type, p: Params)(using first: TypeApplicator[Const[Boolean], Params]): Boolean =
     type_ match
-      case TVar(typeVar) if typeVar == var_ =>
+      case TVar(typeVar) if typeVar == p.var_ =>
         true
-      case TUniv(typeVar, _) if typeVar == var_ =>
+      case TUniv(typeVar, _) if typeVar == p.var_ =>
         false
       case TConstrained(body, bounds) =>
-        body.hasVar(var_) || bounds.map(bound => if bound.var_ == var_ then false else bound.type_.hasVar(var_)).foldM()
+        first.apply(body, p) || bounds.map(bound => if bound.var_ == p.var_ then false else first.apply(bound.type_, p)).foldM()(using AnyMonoid)
       case TConstraining(body, bounds) =>
-        body.hasVar(var_) || bounds.map(_.hasVar(var_)).foldM()
+        first.apply(body, p) || bounds.map(_.hasVar(p.var_)).foldM()(using AnyMonoid)
       case _ =>
-        type_.accumulate(_.hasVar(var_))
+        next.apply(type_, p)
+
+/** Dispatching node of the has variable operation. */
+private object N3 extends TypeDispatcher[Const[Boolean], Const[Boolean], Params](N4):
+      def apply(bounds: List[Bound], p: Params): Boolean =
+        false
+
+/** Monoidal combination node of the has variable operation. */
+private object N4 extends TypeMonoidCombinator[Boolean, Params](AnyMonoid)

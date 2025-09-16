27bcf39c34842ad5d8b227c7ad64c8e7805f100d
Maxime Mulder <maxime-mulder@outlook.com>
2025-09-16 20:01:41 +0800

    sightly clean get variable polarities algorithm

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/impls/getVarPolarities.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/type_/impls/getVarPolarities.scala
@@ -9,40 +9,25 @@ import hkmc2.ctml.util.given
 extension (type_ : Type)
   /** Get the polarities at which a type variable occurs in the type. */
   def getVarPolarities(var_ : TypeVar): Polarities =
-    TypeVarPolarities(type_, TypeVarPolaritiesParams(var_, Polarity.Positive))
+    GetVarPolarities1(type_, GetVarPolaritiesParams(var_, Polarity.Positive))
 
 /** Parameters of the get type variable polarities operation. */
-class TypeVarPolaritiesParams(val var_ : TypeVar, val pol: Polarity) extends WithPolarity[TypeVarPolaritiesParams]:
-  def getPolarity = this.pol
-  def setPolarity(pol: Polarity) = TypeVarPolaritiesParams(var_, pol)
+private class GetVarPolaritiesParams(val var_ : TypeVar, val pol: Polarity) extends WithPolarity[GetVarPolaritiesParams], WithTypeVar[GetVarPolaritiesParams]:
+  def getTypeVar: TypeVar = var_
 
-/** Implementation of the get type variable polarities operation. */
-object TypeVarPolarities extends TypePolarityApplicator[Const[Polarities], Const[Polarities], TypeVarPolaritiesParams](
-  new TypeDispatcher[Const[Polarities], Const[Polarities], TypeVarPolaritiesParams](TypeMonoidCombinator(JoinPolaritiesMonoid)):
-    override def apply(type_ : Type, params: TypeVarPolaritiesParams)(using first: TypeApplicator[Const[Polarities], TypeVarPolaritiesParams] = this): Polarities =
-      type_ match
-        case TVar(var_) if var_ == params.var_ =>
-          Polarities.fromPolarity(params.pol)
-        case _ =>
-          super.apply(type_, params)
+  def setTypeVar(var_ : TypeVar): GetVarPolaritiesParams = GetVarPolaritiesParams(var_, pol)
 
-    def apply(bounds: List[Bound], params: TypeVarPolaritiesParams): Polarities =
-      JoinPolaritiesMonoid.combineMany(
-        bounds.map(bound =>
-          val varPolarities = if bound.var_ == params.var_
-            then Polarities.fromPolarity(params.pol)
-            else Polarities.empty
-          val pol = bound.dir match
-            case Direction.Sub =>
-              params.getPolarity.invert()
-            case Direction.Super =>
-              params.getPolarity
-          val typePolarities = TypeVarPolarities.apply(bound.type_, params.setPolarity(pol))
-          Polarities.join(varPolarities, typePolarities)
-        )
-      )
-):
-  override def bound(bound: Bound, params: TypeVarPolaritiesParams): Polarities =
+  def getPolarity = pol
+  def setPolarity(pol: Polarity) = GetVarPolaritiesParams(var_, pol)
+
+/** Shadowing mode of the "get type variable polarities" operation. */
+private object GetVarPolarities1 extends TypeShadowApplicator[Const[Polarities], GetVarPolaritiesParams](GetVarPolarities2):
+  override def univ(univ: TUniv): Const[Polarities][Type] =
+    Polarities.empty
+
+/** Polarity mode of the "get type variable polarities" operation. */
+private object GetVarPolarities2 extends TypePolarityApplicator[Const[Polarities], Const[Polarities], GetVarPolaritiesParams](GetVarPolarities3):
+  override def bound(bound: Bound, params: GetVarPolaritiesParams): Polarities =
     val varPolarities = if bound.var_ == params.var_
       then Polarities.fromPolarity(params.pol)
       else Polarities.empty
@@ -51,5 +36,30 @@ object TypeVarPolarities extends TypePolarityApplicator[Const[Polarities], Const
         params.getPolarity.invert()
       case Direction.Super =>
         params.getPolarity
-    val typePolarities = TypeVarPolarities.apply(bound.type_, params.setPolarity(pol))
+    val typePolarities = GetVarPolarities1.apply(bound.type_, params.setPolarity(pol))
     Polarities.join(varPolarities, typePolarities)
+
+/** Dispatching mode of the "get type variable polarities" operation. */
+private object GetVarPolarities3 extends TypeDispatcher[Const[Polarities], Const[Polarities], GetVarPolaritiesParams](TypeMonoidCombinator(JoinPolaritiesMonoid)):
+  override def apply(type_ : Type, params: GetVarPolaritiesParams)(using first: TypeApplicator[Const[Polarities], GetVarPolaritiesParams] = this): Polarities =
+    type_ match
+      case TVar(var_) if var_ == params.var_ =>
+        Polarities.fromPolarity(params.pol)
+      case _ =>
+        super.apply(type_, params)
+
+  override def apply(bounds: List[Bound], params: GetVarPolaritiesParams): Polarities =
+    JoinPolaritiesMonoid.combineMany(
+      bounds.map(bound =>
+        val varPolarities = if bound.var_ == params.var_
+          then Polarities.fromPolarity(params.pol)
+          else Polarities.empty
+        val pol = bound.dir match
+          case Direction.Sub =>
+            params.getPolarity.invert()
+          case Direction.Super =>
+            params.getPolarity
+        val typePolarities = GetVarPolarities1.apply(bound.type_, params.setPolarity(pol))
+        Polarities.join(varPolarities, typePolarities)
+      )
+    )

d220cb960e44e46c892561350e538fb48c16e697
Maxime Mulder <maxime-mulder@outlook.com>
2025-09-22 18:46:37 +0800

    use ordered set instead of hashset

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/level.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/level.scala
@@ -1,6 +1,6 @@
 package hkmc2.ctml.core
 
-import scala.collection.mutable.Set as SetMut
+import hkmc2.ctml.util.OrderedSet as MutSet
 import scala.collection.mutable.ListBuffer
 
 import hkmc2.ctml.core.clauses.*
@@ -88,7 +88,7 @@ extension (ctx: Context)
   /** Check whether a type variable is a variable of this level, that is, if it not depended on by
    *  a variable of a lower level.
    */
-  def isLevelVar(var_ : TypeVar, outs: Clauses)(using cache: SetMut[TypeVar] = SetMut()): Boolean =
+  def isLevelVar(var_ : TypeVar, outs: Clauses)(using cache: MutSet[TypeVar] = MutSet()): Boolean =
     // Get the list of type variables that directly depend on the type variable.
     var dependentVars = outs.getDependentVars(var_)
 

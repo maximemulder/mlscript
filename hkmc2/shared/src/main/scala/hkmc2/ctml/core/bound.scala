df86d4592008303a00fe0bee3aacfd40f1996444
Maxime Mulder <maxime-mulder@outlook.com>
2025-12-08 15:20:29 +0800

    fix dumb stack overflow

--- hkmc2/shared/src/main/scala/hkmc2/ctml/core/bound.scala
+++ hkmc2/shared/src/main/scala/hkmc2/ctml/core/bound.scala
@@ -1,8 +1,18 @@
 package hkmc2.ctml.core
 
 import hkmc2.ctml.types.*
+import hkmc2.ctml.core.combine.getExtremalType
 
 extension (bounds: List[Bound])
+  /** Get the type of the leftmost bound of a given type variable in a given direction, or the
+   *  extremal type of that direction if the variable is not bounded in these bounds. */
+  def getVarDirType(var_ : TypeVar, dir: Direction): Type =
+    bounds.find(bound => bound.var_ == var_ && bound.dir == dir) match
+      case Some(bound) =>
+        bound.type_
+      case None =>
+        getExtremalType(dir)
+
   /** Remove the bounds of a variable from the list of bounds. */
   def removeVar(var_ : TypeVar): List[Bound] =
     bounds.filter(_.var_ != var_)
